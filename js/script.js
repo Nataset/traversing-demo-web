f6ffc8369dab663c9bb2a983e4337d478b5c4fb2
Nataset <nataset.t@ku.th>
2021-05-13 22:37:07 +0700

    add reset function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-for (let i = 1; i < 500; i++) {
-    const node = document.createElement("div");
+for (let i = 1; i < 391; i++) {
+    const node = document.createElement("button");
     node.style.width = "100%";
     node.style.height = "100%";
     node.style.border = "2px solid white";
@@ -13,8 +13,15 @@ document.querySelectorAll(".node").forEach((el) => {
     };
 });
 
-let resizeWindow = () => {
-    document.querySelector(".content").style.height = window.innerHeight - 60 + "px";
+document.querySelector("#reset").onclick = () => {
+    document.querySelectorAll(".node").forEach((el) => {
+        el.style.backgroundColor = "black";
+    });
 };
+
+let resizeWindow = () => {
+    document.querySelector(".content").style.height = window.innerHeight - 110 + "px";
+};
+
 resizeWindow();
 window.onresize = resizeWindow;
